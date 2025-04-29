d06164e7316e9a8758117a3cf96ae21577affb02
haimgia <giaoriana24@gmail.com>
2025-04-29 10:10:25 -0400

    added interactive line chart, fixed overlapping labels in final product, removed dual axis chart in final product

--- final_product.py
+++ final_product.py
@@ -4,15 +4,16 @@ from artifacts.side_by_side_line_charts import make_side_line_charts
 from artifacts.bump_chart import make_bump_chart
 from artifacts.racing_bar_chart import make_racing_bar_chart
 from artifacts.dual_axis import make_dual_axis
+from YearlyRevenue.yearly_revenue_interactive import make_racing_line_chart
 import dash
 from dash import dcc, html
 import os
 
 DATASETS_PATH = os.path.join("artifacts", "datasets")
-ASSETS_PATH = os.path.join("YearlyRevenue", "assets")
+ASSETS_PATH = "YearlyRevenue"
 
 
-def initialize_dashboard(datasets_path):
+def initialize_dashboard(datasets_path, assets_path):
     # Initialize the Dash app
     app = dash.Dash(__name__)
 
@@ -25,23 +26,18 @@ def initialize_dashboard(datasets_path):
             dcc.Graph(figure=make_side_line_charts()),
             dcc.Graph(figure=make_bump_chart(datasets_path)),
             dcc.Graph(figure=make_racing_bar_chart(datasets_path)),
-            dcc.Graph(figure=make_dual_axis(datasets_path)),
+            #dcc.Graph(figure=make_dual_axis(datasets_path)),
+            dcc.Graph(figure=make_racing_line_chart(assets_path)),
         ], style={
             'display': 'block',
             'maxHeight': '90vh', 
             'overflowY': 'auto',
             'padding': '20px'
-        }),
-        html.Video(
-            src=os.path.join(ASSETS_PATH, "yearly_revenue_animation.mp4"),
-            controls=True,
-            autoPlay=False,
-            style={"width": "100%", "maxWidth": "900px", "margin": "auto", "display": "block"}
-        )
+        })
     ])
 
     return app
 
 if __name__ == "__main__":
-    app = initialize_dashboard(DATASETS_PATH)
+    app = initialize_dashboard(DATASETS_PATH, ASSETS_PATH)
     app.run(debug=True)
